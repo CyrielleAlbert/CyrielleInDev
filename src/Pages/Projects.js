60f1023d36000f9212c5d67e1b1f0c7108550ca0
Cyrielle Albert <cyrielle.albert@reseau.eseo.fr>
2020-11-26 22:02:36 +0100

    Projets page

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -1,8 +1,26 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import { NavLink } from "react-router-dom";
 import ProjectsInfo from "./Projects/ProjectsInfo.json";
 
 class Projects extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      screenWidth: window.innerWidth,
+      show: {},
+    };
+  }
+  updateScreenSize = () => {
+    this.setState({ screenWidth: window.innerWidth });
+    console.log(this.state.screenWidth);
+  };
+  componentDidMount() {
+    window.addEventListener("resize", this.updateScreenSize);
+  }
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updateScreenSize);
+  }
+
   render() {
     return (
       <div style={{ backgroundColor: "#FFFDFA" }}>
@@ -11,7 +29,7 @@ class Projects extends Component {
             style={{
               color: "#FFFDFA",
               fontFamily: "MontSerrat",
-              fontWeight:'bold',
+              fontWeight: "bold",
               color: "black",
               fontSize: 60,
               textAlign: "center",
@@ -22,7 +40,7 @@ class Projects extends Component {
               style={{
                 fontFamily: "Montserrat",
                 fontSize: 30,
-                fontWeight:'lighter',
+                fontWeight: "lighter",
                 color: "#000000",
               }}
             >
@@ -52,50 +70,183 @@ class Projects extends Component {
             </p>
           </p>
         </div>
-        <div style={{ paddingBottom: 300 }}>
-          {Object.keys(ProjectsInfo).map((id) => {
-            console.log(id)
-            return (
-              <NavLink
-                to={{
-                  pathname: "/Projects/" +  id ,
-                }}
-                style={{ textDecoration: "none" }}
-              >
+        {this.state.screenWidth > 900 && (
+          <div
+            style={{
+              display: "flex",
+              flexWrap: "wrap",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            {Object.keys(ProjectsInfo).map((id) => {
+              console.log(id);
+              return (
                 <div
+                  onMouseEnter={() => {
+                    this.setState({ show: { [id]: true } });
+                    console.log(this.state.show);
+                  }}
+                  onMouseLeave={() => {
+                    console.log(this.state.show);
+                    this.setState({ show: { [id]: false } });
+                  }}
                   style={{
-                    flexDirection: "row",
-                    display: "flex",
-                    marginRight: "20%",
-                    marginLeft: "20%",
-                    marginTop: 100,
-                    boxShadow: "0px 0px 10px #C4C2C0",
-                    marginBottom: 100,
+                    marginBottom: 50,
+                    marginLeft: 10,
+                    marginRight: 10,
+                    flex: "1 1 30%",
+                    textAlign: "center",
+                    position: "relative",
+                    boxShadow: "0px 4px 13px rgba(0, 0, 0, 0.25)",
                   }}
                 >
-                  <div style={{ width: "60%", height: "auto" }}>
-                    <img src={ProjectsInfo[id].imagePath} width={"100%"} />
-                  </div>
+                  <img
+                    src={ProjectsInfo[id].imagePath}
+                    width={"100%"}
+                    style={{
+                      transition: "filter 0.3s",
+                      filter: this.state.show[id] ? "blur(4px)" : "blur(0)",
+                    }}
+                  />
+                  {this.state.show[id] && (
+                    <div
+                      style={{
+                        width: "100%",
+                        opacity: "100%",
+                        position: "absolute",
+                        top: 0,
+                        right: 0,
+                        width: "100%",
+                        height: "100%",
+                      }}
+                    >
+                      <NavLink
+                        to={{
+                          pathname: "/Projects/" + id,
+                        }}
+                        style={{
+                          textDecoration: "none",
+                          padding: 5,
+                        }}
+                      >
+                        <div
+                          style={{
+                            display: "flex",
+                            height: "60%",
+                            width: "60%",
+                            marginTop: "10%",
+                            color: "black",
+                            backgroundColor: "white",
+                            marginLeft: "20%",
+                            padding: 15,
+                            font: "Montserrat",
+                            fontSize: 30,
+                            fontWeight: "bold",
+                            boxShadow: "0px 4px 13px rgba(0, 0, 0, 0.25)",
+                            alignContent: "center",
+                            justifyContent: "center",
+                            alignItems: "center",
+                          }}
+                        >
+                          {ProjectsInfo[id].title}
+                        </div>
+                      </NavLink>
+                    </div>
+                  )}
+                </div>
+              );
+            })}
+          </div>
+        )}
+        {this.state.screenWidth <= 900 && (
+          <div
+            style={{
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            {Object.keys(ProjectsInfo).map((id) => {
+              console.log(id);
+              return (
+                <div
+                  style={{
+                    marginBottom: 50,
+                    marginLeft: 10,
+                    marginRight: 10,
+                    width: "60%",
+                    marginLeft: "20%",
+                    textAlign: "center",
+                    position: "relative",
+                    boxShadow: "0px 4px 13px rgba(0, 0, 0, 0.25)",
+                  }}
+                >
+                  <img
+                    src={ProjectsInfo[id].imagePath}
+                    width={"100%"}
+                    style={{
+                      transition: "filter 0.3s",
+                      filter: "blur(4px)",
+                    }}
+                  />
                   <div
                     style={{
-                      width: "40%",
-                      height: "auto",
-                      color: "black",
-                      fontSize: 40,
-                      fontFamily: "Montserrat",
-                      fontWeight: "bold",
-                      textAlign: "center",
-                      alignItems:'center',
-                      justifyContent:'center',
-                      backgroundColor:'white'
+                      width: "100%",
+                      opacity: "100%",
+                      position: "absolute",
+                      top: 0,
+                      right: 0,
+                      width: "100%",
+                      height: "100%",
                     }}
                   >
-                    {ProjectsInfo[id].title}
+                    <NavLink
+                      to={{
+                        pathname: "/Projects/" + id,
+                      }}
+                      style={{
+                        textDecoration: "none",
+                        padding: 5,
+                      }}
+                    >
+                      <div
+                        style={{
+                          display: "flex",
+                          height: "60%",
+                          width: "60%",
+                          marginTop: "10%",
+                          color: "black",
+                          backgroundColor: "white",
+                          marginLeft: "20%",
+                          padding: 15,
+                          font: "Montserrat",
+                          fontSize: 30,
+                          fontWeight: "bold",
+                          boxShadow: "0px 4px 13px rgba(0, 0, 0, 0.25)",
+                          alignContent: "center",
+                          justifyContent: "center",
+                          alignItems: "center",
+                        }}
+                      >
+                        {ProjectsInfo[id].title}
+                      </div>
+                    </NavLink>
                   </div>
                 </div>
-              </NavLink>
-            );
-          })}
+              );
+            })}
+          </div>
+        )}
+        <div
+          style={{
+            color: "black",
+            textAlign: "center",
+            opacity: 0.1,
+            fontSize: 35,
+            marginBottom: 50,
+          }}
+        >
+          More to come...
         </div>
         <div
           style={{
